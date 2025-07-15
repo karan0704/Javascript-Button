322d45f8dd76444767c6b83cda0b483e25e6f6e0
Karan M <karanmohokar7@gmail.com>
2025-07-15 10:29:34 +0530

    add confirmReset

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ clickbutton.addEventListener("click", function () {
 const resetButton = document.getElementById("resetButton");
 
 resetButton.addEventListener("click", function () {
+  const confirmReset = confirm("Are you sure you want to reset the count?");
+  if (confirmReset) {
   count = 0;
   countDisplay.textContent = count;
+  }
 });
